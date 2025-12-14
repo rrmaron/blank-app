7057b124cbe4b0716ee0ef9db49e4f11a3f956ac
rrmaron <46344121+rrmaron@users.noreply.github.com>
2025-12-14 10:22:19 -0600

    Update streamlit_app.py
    make pdf generator work

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import base64
 # Auto-install reportlab
 from reportlab.lib.pagesizes import A4
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib import colors
 from reportlab.lib.units import inch
 
