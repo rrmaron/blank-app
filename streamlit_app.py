3b0529c3e9af8557ac9a73fd14870ce78846a8dd
rrmaron <46344121+rrmaron@users.noreply.github.com>
2026-01-03 21:44:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -132,7 +132,10 @@ st.markdown("<h1 style='text-align: center; color: #003087;'>FIDE Initial Rating
 # === EXPLANATORY PARAGRAPH UNDER HEADING ===
 st.markdown("""
 <p style='text-align: center; color: #555; font-size: 1.1em; margin: 0 0 1rem 0;'>
-To calculate the initial rating, it starts with 2 draws against fictional opponents that have a 1800 rating — these are prefilled below. Then we need 5 more results against FIDE-rated opponents to get your first official rating.  Only start entering results after you have at least draen against a FIDE rated opponent, so FIDE tournaments where you have earned 0 points do not count until you have at least drawn against a FIDE rated opponemt.
+To calculate the initial rating, it starts with 2 draws against fictional opponents that have a 1800 rating — these are prefilled below. 
+Then we need 5 more results against FIDE-rated opponents to get your first official rating.  Only start entering results after you have 
+at least drawn against a FIDE rated opponent, so FIDE tournaments where you have earned 0 points do not count until you have at least drawn 
+against a FIDE rated opponemt.
 </p>
 """, unsafe_allow_html=True)
 
